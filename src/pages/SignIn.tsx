bb6a15cc649d47b544c8d8765d5419927f2c3e8f
lupo tobi <daniellupo30@gmail.com>
2025-05-24 21:43:01 +0100

    sign in updates

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,10 +1,17 @@
-
 import { SignIn } from "@clerk/clerk-react";
 
 const SignInPage = () => {
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL 
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <SignIn path="/sign-in" routing="path"  fallbackRedirectUrl={`/manuals`}/>
+      <SignIn 
+        path="/sign-in" 
+        routing="path" 
+        fallbackRedirectUrl={`${baseUrl}/manuals`}
+      />
     </div>
   );
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,17 @@
-
 import { SignUp } from "@clerk/clerk-react";
 
 const SignUpPage = () => {
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL 
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <SignUp path="/sign-up" routing="path"  fallbackRedirectUrl={`/manuals`}/>
+      <SignUp 
+        path="/sign-up" 
+        routing="path" 
+        fallbackRedirectUrl={`${baseUrl}/manuals`}
+      />
     </div>
   );
 };
